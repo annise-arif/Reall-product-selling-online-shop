d28e50be7eae11d27327d13f92b388dbee855f6e
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-03-26 02:10:08 +0600

    added-header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1> Reall Product Selling <span>Online Shop</span></h1>
+            <h3>Chose Some Items To Buy From Here</h3>
+        </div>
+    );
+};
+
+export default Header;
